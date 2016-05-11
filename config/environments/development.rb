df002c8f2ce1855d561882d7c32b79cc256b1477
seanreed1111 <seanreed1111@gmail.com>
2016-05-11 09:58:43 -0400

    changes links to images to include subdir portfolio in the asset pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #serve static files here
+  config.serve_static_files = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
